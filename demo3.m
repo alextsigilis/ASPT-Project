2c9bfdcda5e41cacb36649c942afe15fbba04018
cmichailidis <cmichailidis98@gmail.com>
2022-07-30 07:03:36 -0400

    first working version

--- demo3.m
+++ demo3.m
@@ -35,15 +35,38 @@ clear all; close all; clc;
 % (Choose any value that you want to experiment with)
 % ======================================================
 
-input_file = "SN001.edf";    % name of input file
-channel = 1;                 % index of selected channel
-w = duration("00:00:03");    % window length when estimating moments
-wavelet = "db5";             % wavelet for MRA 
-levels = 7;                  % number of levels on MRA binary tree 
-pivot = 5;                   % Use every frequency scale below pivot  
-                             % (including the pivot scale) for 
-                             % signal reconstruction
+% name of input file
+input_file = "SN001.edf";            
 
+% name of annotations file
+annot_file = "SN001_sleepscoring.edf";
+
+% index of selected channel
+% index: signal label
+% 1:     EEG F4-M1
+% 2:     EEG C4-M1
+% 3:     EEG O2-M1
+% 4:     EEG C3-M2
+% 5:     EMG chin
+% 6:     EOG E1-M2
+% 7:     EOG E2-M2
+% 8:     ECG
+channel = 1;
+
+% window lenth when estimating higher order statistics
+w = duration("00:00:01");
+
+% wavelet for MRA
+wavelet = "db5";
+
+% number of levels on MRA binary tree
+levels = 7;                          
+
+% Use every frequency scale below 
+% pivot (including pivot) for signal
+% reconstruction 
+pivot = 5;                         
+                                       
 % ------------------------------------------------------
 % Do not change anything below that point, 
 % unless you know exactly what you are doing.
@@ -61,7 +84,11 @@ fprintf("Loading input file ...  ");
 % Read the entire EDF file and
 % choose the appropriate channel
 X = edfread(input_file);
-sig = X{:,channel};    
+sig = X{:,channel};
+
+% Read the annotations from the 
+% sleep scoring EDF file
+[~,labels] = edfread(annot_file);
 
 % Read file metadata
 info = edfinfo(input_file);
@@ -83,7 +110,7 @@ time = linspace(0, N*d, N*d*fs);
 sig = cell2mat(sig);       
 
 % delete unused variables
-clear X;
+% clear X;
 
 fprintf("Done\n\n");
 
@@ -137,43 +164,63 @@ l = w * fs;
 % number of segments created by sliding window
 K = N * d * fs / l;
 
+% frequency boundaries for every analysis scale
+freq = (fs/2) * 2.^-[0:levels];
+freq = [freq 0];
+
 % Plots of selected frequency scales
 % and sliding window statistics
 for i = pivot:1:num_of_scales
     figure(i); t = tiledlayout(4,1);
     
+    % f1: lower bound of frequency scale in Hertz
+    % f2: upper bound of frequency scale in Hertz
+    f1 = freq(i+1);
+    f2 = freq(i+0);
+
     x = mra(i,1:K*l);
     x = reshape(x, K, l);
 
     % estimate higher order moments
-    m   = sum(x,2) / l;
-    var = sum((x-m).^2, 2) / l;
-    std = sqrt(var);
-    skw = sum((x-m).^3, 2) / l;
-    krt = sum((x-m).^4, 2) / l;
-    skw = skw ./ (std.^3);
-    krt = krt ./ (std.^4);
+    m   = sum(x,2) / l;            % sliding average 
+    var = sum((x-m).^2, 2) / l;    % sliding variance
+    std = sqrt(var);               % sliding standard deviation
+    skw = sum((x-m).^3, 2) / l;    % sliding 3rd moment
+    krt = sum((x-m).^4, 2) / l;    % sliding 4th moment
+    skw = skw ./ (std.^3);         % sliding skewness
+    krt = krt ./ (std.^4);         % sliding kurtosis
 
-    % subplot of reconstructed signal
+    % upsample to match the length of the 
+    % current frequency scale
+    std = interp1(1:1:K, std, (1:1:K*l)/l);
+    skw = interp1(1:1:K, skw, (1:1:K*l)/l);
+    krt = interp1(1:1:K, krt, (1:1:K*l)/l);
+
+    % subplot of current frequency scale
     x1 = nexttile;
-    plot(x1, time(1:K*l), sig1(1:K*l));
+    plot(x1, time(1:K*l), mra(i,1:K*l));
     xlabel('time in seconds'); 
-    ylabel('Amplitude');
+    ylabel(sprintf('Scale %d', i));
+    title(sprintf('%.2fHz-%.2fHz', f1, f2));
     
     % subplot of sliding variance
     x2 = nexttile;
-    plot(x2, var);           
-    ylabel('Variance');
+    plot(x2, time(1:K*l), std);
+    xlabel('time in seconds');
+    ylabel('Std. Deviation');
 
     % subplot of sliding skewness
     x3 = nexttile;             
-    plot(x3, skw);                 
+    plot(x3, time(1:K*l), skw);
+    xlabel('time in seconds');
     ylabel('Skewness');
 
     % subplot of sliding kurtosis
     x4 = nexttile;                     
-    plot(x4, krt);           
+    plot(x4, time(1:K*l), krt); 
+    xlabel('time in seconds');
     ylabel('Kurtosis');
 
+    % Link time axes
     linkaxes([x1 x2 x3 x4], 'x');
 end
