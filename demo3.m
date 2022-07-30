a35fc479dbd2f2ae9e8471d54937f702752183a9
cmichailidis <cmichailidis98@gmail.com>
2022-07-30 01:38:17 -0400

    bug fix

--- demo3.m
+++ demo3.m
@@ -1,9 +1,4 @@
 % ======================================================
-% WARNING: Don't even bother running this script.
-% There are many bugs that need to be fixed.
-% ======================================================
-
-% ======================================================
 % Demo Script (3)
 % ------------------------------------------------------
 %
@@ -16,7 +11,7 @@
 % recording of an EEG/EOG/ECG channel
 %
 % 2) Choose the most appropriate frequency scales
-% to reconstruct an approximation of the orignal
+% to reconstruct an approximation of the original
 % recording
 % 
 % 3) Estimate 2nd, 3rd and 4th order moments
@@ -32,22 +27,22 @@
 % by visual inspection of the waveforms.
 % ======================================================
 
-clear all; clc;     % reset your workspace and clear terminal
+% reset your workspace and clear terminal
+clear all; close all; clc;
 
 % ======================================================
 % 1) Parameters of MRA Decomposition and Reconstruction
 % (Choose any value that you want to experiment with)
 % ======================================================
 
-input_file = "SN001.edf";           % name of input file
-channel = "EEGF4_M1";               % name of selected channel
-win_length = duration("00:00:03");  % window length when estimating moments
-wavelet = "db5";                    % wavelet for MRA 
-levels = 7;                         % number of levels on MRA binary tree 
-fs = 256;                           % sampling frequency of the recording
-pivot = 5;                          % Use every frequency scale below pivot  
-                                    % (including the pivot scale) for 
-                                    % signal reconstruction
+input_file = "SN001.edf";    % name of input file
+channel = 1;                 % index of selected channel
+w = duration("00:00:03");    % window length when estimating moments
+wavelet = "db5";             % wavelet for MRA 
+levels = 7;                  % number of levels on MRA binary tree 
+pivot = 5;                   % Use every frequency scale below pivot  
+                             % (including the pivot scale) for 
+                             % signal reconstruction
 
 % ------------------------------------------------------
 % Do not change anything below that point, 
@@ -57,27 +52,46 @@ pivot = 5;                          % Use every frequency scale below pivot
 % number of frequency scales
 num_of_scales = levels + 1;                
 
-% frequency boundaries for every scale
-scales = (fs/2) * 2 .^ 1:1:num_of_scales;
-scales = [scales 0];
-
-% time axis for EEG/EOG/ECG signal
-time = linspace(0, ...);             % TODO: fix that shit
-
 % ======================================================
 % 2) Read data from the EDF file
 % ======================================================
 
-X = edfread("SN001.edf");        % Read the entire file as a timetable
-sig = X{:,channel};              % choose the appropriate channel
-sig = cell2mat(sig);             % convert chosen channel to 1D-array
+fprintf("Loading input file ...  ");
 
-clear X;                         % delete the timetable to 
-                                 % save some space in RAM
+% Read the entire EDF file and
+% choose the appropriate channel
+X = edfread(input_file);
+sig = X{:,channel};    
+
+% Read file metadata
+info = edfinfo(input_file);
+
+% N:  number of data records per recording
+% d:  duration of every data record in seconds
+% n:  samples per data record
+% fs: sampling frequency in Hertz
+N = info.NumDataRecords;
+d = seconds(info.DataRecordDuration);
+n = info.NumSamples(channel);
+fs = n / d;
+
+% construct a time axis for
+% the EEG/EOG/ECG channel
+time = linspace(0, N*d, N*d*fs);
+
+% cell-array -> 1D array
+sig = cell2mat(sig);       
+
+% delete unused variables
+clear X;
+
+fprintf("Done\n\n");
 
 % ======================================================
 % 3) Perform MRA decomposition and reconstruction
 % ======================================================
+
+fprintf("Performing MRA ... ");
 
 % Logical array for selecting reconstruction scales
 levelForReconstruction = (1:1:num_of_scales) >= pivot;
@@ -91,50 +105,19 @@ mra = modwtmra(wt,wavelet);
 % Sum down the rows of the selected multiresolution signals
 sig1 = sum(mra(levelForReconstruction,:),1);
 
-% Delete unused variables to save some space on RAM
+% Delete unused variables
 clear wt;  
 
-% ======================================================
-% 4) Estimate variance, skewness and kurtosis in a 
-% sliding window 
-% ======================================================
-
-% Convert the reconstructed signal 
-% into a 2D array.
-% ---------------------------------------
-% Every row of this 2D array contains
-% a small window  of the reconstructed
-% signal where variance, skewness and
-% kurtosis are locally estimated.
-% ---------------------------------------
-% The duration of the window is adjusted
-% in section 1) of the script 
-
-n = fs * win_length;            % size of sliding window in samples
-
-x = reshape(sig1, ...);         % TODO: fix that shit
-
-m   = sum(x, 1) / n;            % sliding average
-var = sum((x-m).^2, 1) / n;     % sliding variance (biased estimate) 
-std = sqrt(var);                % sliding standard deviation
-
-skw = sum((x-m).^3, 1) / n;     % sliding 3rd moment
-krt = sum((x-m).^4, 1) / n;     % sliding 4th moment
-
-skw = skw ./ (std.^3);          % sliding skewness (biased estimate)
-krt = krt ./ (std.^4);          % sliding kurtosis (biased estimate)
-
-% delete unused variables
-clear x, avg;                 
+fprintf("Done\n\n");
 
 % ======================================================
-% 5) Plot the results
-% ======================================================
+% 4) Plot of Original vs Reconstructed Waveform
+% ======================================================            
 
 % Reconstructed vs Original Signal
 figure(1); 
 
-plt = plot(time,sig, time, sig2);
+plt = plot(time,sig, time,sig1);
 plt(1).LineWidth = 0.5; 
 plt(2).LineWidth = 2.0;
 
@@ -142,48 +125,55 @@ xlabel("time in seconds");
 ylabel("Amplitude in microVolts");
 title("Original vs Reconstruction");
 
-% ------------------------------------------------------
+% ======================================================
+% Estimate variance/skewness/kurtosis
+% in a sliding window.
+% ======================================================
+
+% length of sliding window in samples
+w = seconds(w);
+l = w * fs;
+
+% number of segments created by sliding window
+K = N * d * fs / l;
 
 % Plots of selected frequency scales
 % and sliding window statistics
 for i = pivot:1:num_of_scales
-    figure(i);
+    figure(i); t = tiledlayout(4,1);
+    
+    x = mra(i,1:K*l);
+    x = reshape(x, K, l);
 
-    % upsample std(i,:), var(i,:) skw(i,:) 
-    % and krt(i,:) in order to match the 
-    % length of the reconstructed signal
-    std = interp1(std(i,:), time);
-    var = interp1(var(i,:), time);
-    skw = interp1(skw(i,:), time);
-    krt = interp1(krt(i,:), time);
+    % estimate higher order moments
+    m   = sum(x,2) / l;
+    var = sum((x-m).^2, 2) / l;
+    std = sqrt(var);
+    skw = sum((x-m).^3, 2) / l;
+    krt = sum((x-m).^4, 2) / l;
+    skw = skw ./ (std.^3);
+    krt = krt ./ (std.^4);
 
     % subplot of reconstructed signal
-    subplot(5,1,1);
-    plot(time, sig1);
-    xlabel('time in seconds'); 
-    ylabel('Amplitude');
-    
-    % subplot of frequency scale
-    subplot(5,1,2);        
-    plot(time, mra(i,:));
+    x1 = nexttile;
+    plot(x1, time(1:K*l), sig1(1:K*l));
     xlabel('time in seconds'); 
     ylabel('Amplitude');
     
     % subplot of sliding variance
-    subplot(5,1,3);
-    plot(time, var);           
-    xlabel('time in seconds'); 
+    x2 = nexttile;
+    plot(x2, var);           
     ylabel('Variance');
 
     % subplot of sliding skewness
-    subplot(5,1,4);              
-    plot(time, skw);                
-    xlabel('time in seconds'); 
+    x3 = nexttile;             
+    plot(x3, skw);                 
     ylabel('Skewness');
 
     % subplot of sliding kurtosis
-    subplot(5,1,5);                      
-    plot(time, krt);           
-    xlabel('time in seconds'); 
+    x4 = nexttile;                     
+    plot(x4, krt);           
     ylabel('Kurtosis');
+
+    linkaxes([x1 x2 x3 x4], 'x');
 end
