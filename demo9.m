08f01dbc80fdc5004a6bd539d7619860143504b8
cmichailidis <cmichailidis98@gmail.com>
2022-08-22 03:30:22 +0300

    added docstring

---
+++ demo9.m
@@ -0,0 +1,59 @@
+% =============================================================
+% Author: Christodoulos Michaelides
+% Date: August 22nd, 2022
+% -------------------------------------------------------------
+% 
+% Demo Script 9
+%
+% 1) Choose a patient and an EEG recording from the dataset
+% 2) Estimate the bicoherence of the EEG in every 30sec epoch
+% 3) Plot the bicoherence estimations (pause for a few seconds
+%    between each plot)
+% =============================================================
+
+% =============================================================
+% 0) Reset your workspace variables
+% =============================================================
+clear all; close all; clc;
+
+% =============================================================
+% 1) Script parameters (Choose any value you want)
+% =============================================================
+
+patient_ID = 1;     % integer 1 ... 151
+channel_ID = 1;     % integer 1 ... 4
+
+% Parameters for bicoherence estimator
+K  = 32;            % number of partitions per 30sec epoch
+fs = 256;           % EEG sampling frequency in Hertz
+fc = 32;            % cut-off frequency for bicoherence estimation
+
+% Plot settings
+dt = 1.0;           % Playback speed in seconds per frame     
+levels = 8;         % number of levels for contour plots
+
+% =============================================================
+% 2) Bispectrum estimation
+% =============================================================
+
+fprintf("Loading EDF file ... "); tic;
+Z = loadEDF(patient_ID);
+fprintf("Done\n"); toc;
+
+fprintf("Estimating bicoherence index ... "); tic;
+[bic, freq] = bicEEG(Z,fs,fc,K,channel_ID);
+fprintf("Done"); toc;
+
+% =============================================================
+% 3) Bicoherence Plots
+% =============================================================
+
+N = size(bic,1);
+pause('on');
+
+for i = 1:1:N
+    txt = sprintf("frame %d out of %d: %s",i,N,bic.Annotations{i});
+    contourf(freq,freq,cell2mat(bic{i,1}),levels,'LineColor','none');
+    xlabel('f_1'); ylabel('f_2'); title(txt);
+    pause(dt);
+end
