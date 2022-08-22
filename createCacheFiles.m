04a48daaa6ea3670b452e9fe7760c2a51d83030f
cmichailidis <cmichailidis98@gmail.com>
2022-08-22 21:09:41 +0300

    Initial file commit

---
+++ createCacheFiles.m
@@ -0,0 +1,42 @@
+% =============================================================
+% Author: Christodoulos Michaelides
+% Date: August 22nd, 2022
+% -------------------------------------------------------------
+%
+% Description:
+% Reading EDF/EDF+ files seems to be extremely inefficient
+% when using the builtin edfread() and edfinfo() functions.
+% In our case things are made worse because of the fact that 
+% patients have two EDF files associated with them. The first
+% contains the EEG recordings and the second contains the sleep 
+% stage Annotations. 
+% 
+% On the other hand, loading data from .mat files is 
+% significantly faster. This script reads EEG recordings and 
+% sleep stage Annotations from the EDF files and stores them
+% in .mat files to speed up file I/O operations in the future.
+%
+% You don't have to manually load data from those .mat files. 
+% Instead, loadEDF always looks for them before attempting to 
+% read EDF files.
+% =============================================================
+
+clear all; close all; clc;
+
+start = 51;          % first patient
+stop  = 53;          % last patient
+
+for i = start:1:stop
+    matFile = sprintf("%03d.mat",i);
+    edfFile = sprintf("SN%03d.edf",i);
+
+    if ~isfile(edfFile)
+        continue;
+    end
+
+    fprintf("Patient %d out of %d\n", i, stop);
+    Z = loadEDF(i);
+    save(matFile, 'Z');
+end
+
+clear all;
