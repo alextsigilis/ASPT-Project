d1295c0d8d4a4126b60989a5aceccfecc336d951
cmichailidis <cmichailidis98@gmail.com>
2022-08-27 00:08:31 +0300

    fixed normalization issues with entropy estimations

--- bicoherFeatures.m
+++ bicoherFeatures.m
@@ -13,7 +13,7 @@
 %
 % Arguments List: (X, freq)
 % X: (table) a table with two columns. The first should
-% contain the bispectrum/bicoherence estimations and the
+% contain the bicoherence estimations and the
 % second should contain the sleep stage Annotations.
 % You should use bicEEG to obtain this table.
 % -------------------------------------------------------
@@ -27,8 +27,8 @@ function [Y] = bicoherFeatures(X)
     % Number of EEG segments (30sec epochs)
     N = size(X,1);
 
-    % Initialize an empty table to store features
-    % from the bicoherence / bispectrum matrices.
+    % Initialize an empty table to store
+    % features from the bicoherence matrices.
     types = ["double", "double", "double", "string"];
     names = ["ent1", "ent2", "ent3", "Annotations"];
 
@@ -38,23 +38,17 @@ function [Y] = bicoherFeatures(X)
         'VariableNames', names);
 
     for i = 1:1:N
-        % Extract the entire bicoherence / bispectrum
+        % Extract the entire bicoherence
         % matrix for a 30sec EEG segment
         bic = cell2mat(X{i,1});
         
-        % feature extraction from 
-        % bicoherence/bispectrum matrix 
-        p = (bic.^1) / sum(bic(:).^1); 
-        q = (bic.^2) / sum(bic(:).^2);
-        r = (bic.^3) / sum(bic(:).^3);
-
-        Y{i,"ent1"} = - sum(p(:).*log2(p(:)),'omitnan');
-        Y{i,"ent2"} = - sum(q(:).*log2(q(:)),'omitnan');
-        Y{i,"ent3"} = - sum(r(:).*log2(r(:)),'omitnan');
-
-        % Y{i,"H1"} = nan;
-        % Y{i,"H2"} = nan;
-        % Y{i,"H3"} = nan;
+        % Estimate bicoherence entropies 
+        p = bic(:).^1; 
+        Y{i,"ent1"} = -sum(p(:).*log2(p(:)),'omitnan');
+        q = bic(:).^2; 
+        Y{i,"ent2"} = -sum(q(:).*log2(q(:)),'omitnan');
+        r = bic(:).^3; 
+        Y{i,"ent3"} = -sum(r(:).*log2(r(:)),'omitnan');
     end
 
     % Copy sleep stage Annotations;
