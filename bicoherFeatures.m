1817c6fce04393777bc62b40d79683c9a0229a60
cmichailidis <cmichailidis98@gmail.com>
2022-09-01 04:42:07 +0300

    final commit (excluding future code optimizations)

--- bicoherFeatures.m
+++ bicoherFeatures.m
@@ -8,15 +8,18 @@
 % bicoherence estimations of EEG signals such as:
 %   1) bicoherence entropy
 %   2) bicoherence squared-entropy
-%   4) average bicoherence
-%   5) maximum bicoherence
+%   3) average bicoherence
 % -------------------------------------------------------
 %
-% Arguments List: (X, freq)
+% Arguments List: (X, f)
 % X: (table) a table with two columns. The first should
 % contain the bicoherence estimations and the
 % second should contain the sleep stage Annotations.
-% You should use bicEEG to obtain this table.
+% You should use bicEEG() to obtain this table 
+%
+% f: (1D array of floats) the frequency axis of the
+% bicoherence matrices. You should use bicEEG to 
+% obtain this vector.
 % -------------------------------------------------------
 %
 % Return List: (delta, theta, alpha, beta)
@@ -24,80 +27,31 @@
 % TODO: add text
 % =======================================================
 
-function [delta, theta, alpha, beta] = bicoherFeatures(X, f)
+function [Y] = bicoherFeatures(X, f)
     % Number of EEG segments (30sec epochs)
     N = size(X,1);
 
     % Initialize empty tables to store
     % features from the bicoherence matrices.
-    types = ["double" "double" "double" "double" "string"];
-    names = ["ent1" "ent2" "avg" "argmax" "Annotations"];
+    types = ["double" "double" "double" "string"];
+    names = ["ent1" "ent2" "avg" "Annotations"];
     sz = [N numel(types)];
 
-    delta = table('Size',sz,'VariableTypes',types,'VariableNames',names);
-    theta = table('Size',sz,'VariableTypes',types,'VariableNames',names);
-    alpha = table('Size',sz,'VariableTypes',types,'VariableNames',names);
-    beta  = table('Size',sz,'VariableTypes',types,'VariableNames',names);
-
-    % Binary masks for splitting the bicoherence matrix
-    % into delta, theta, alpha and beta wave partitions
-    f0 = 0.0; f1 = 4.0; f2 = 8.0; f3 = 16.0; f4 = 32.0;
-    deltaHex = (f'<=f) & (f>=0) & f0<=(f+f') & (f+f')<=f1;
-    thetaHex = (f'<=f) & (f>=0) & f1<=(f+f') & (f+f')<=f2;
-    alphaHex = (f'<=f) & (f>=0) & f2<=(f+f') & (f+f')<=f3;
-    betaHex  = (f'<=f) & (f>=0) & f3<=(f+f') & (f+f')<=f4;
+    Y = table('Size',sz,'VariableTypes',types,'VariableNames',names);
 
     for i = 1:1:N
         % Extract the entire bicoherence
         % matrix for a 30sec EEG segment
         bic = cell2mat(X{i,1});
 
-        % Partition the bicoherence matrix
-        deltaBic = bic(deltaHex); 
-        thetaBic = bic(thetaHex); 
-        alphaBic = bic(alphaHex);
-        betaBic  = bic(betaHex);
-        
-        % Extract features from delta waves
-        p = deltaBic.^1; 
-        q = deltaBic.^2; 
-
-        delta{i,"ent1"} = -mean(p.*log2(p),'omitnan');
-        delta{i,"ent2"} = -mean(q.*log2(q),'omitnan');
-        delta{i,"avg"}  = +mean(deltaBic);
-        delta{i,"argmax"} = 0;
-
-        % Extract features from theta waves
-        p = thetaBic.^1; 
-        q = thetaBic.^2; 
-        
-        theta{i,"ent1"} = -mean(p.*log2(p),'omitnan');
-        theta{i,"ent2"} = -mean(q.*log2(q),'omitnan');
-        theta{i,"avg"}  = +mean(thetaBic);
-        theta{i,"argmax"} = 0;
-
-        % Extract features from alpha waves
-        p = alphaBic.^1; 
-        q = alphaBic.^2; 
-
-        alpha{i,"ent1"} = -mean(p.*log2(p),'omitnan');
-        alpha{i,"ent2"} = -mean(q.*log2(q),'omitnan');
-        alpha{i,"avg"}  = +mean(alphaBic);
-        alpha{i,"argmax"} = 0;
-
-        % Extract features from beta waves
-        p = betaBic.^1; 
-        q = betaBic.^2; 
-        
-        beta{i,"ent1"} = -mean(p.*log2(p),'omitnan');
-        beta{i,"ent2"} = -mean(q.*log2(q),'omitnan');
-        beta{i,"avg"}  = +mean(betaBic);
-        beta{i,"argmax"} = 0;
-
-        % Copy sleep stage annotations
-        delta{i, "Annotations"} = X{i, "Annotations"};
-        theta{i, "Annotations"} = X{i, "Annotations"};
-        alpha{i, "Annotations"} = X{i, "Annotations"};
-        beta{i,  "Annotations"} = X{i, "Annotations"};
+        % Extract features from the
+        % bicoherence matrix
+        p = bic(:).^1; q = bic(:).^2;
+        Y{i,"avg"}  = + mean(bic(:));
+        Y{i,"ent1"} = - mean(p .* log2(p),'omitnan');
+        Y{i,"ent2"} = + log2(1 - 7.0 * mean(q .* log2(q),'omitnan'));
     end
+
+    % Copy sleep stage annotations
+    Y.Annotations = X.Annotations;
 end
--- demo10.m
+++ demo10.m
@@ -1,3 +1,17 @@
+% ===================================================================
+% Author: Christodoulos Michaelides
+% Date: August 25th, 2022
+% -------------------------------------------------------------------
+%
+% Description:  
+%
+% This script estimates the EEG-bicoherence matrices on a large 
+% number of patients. It then extracts features from the bicoherence 
+% estimates and plots histograms of those features. Different
+% histograms are generated for every sleep stage. We can identify
+% useful features by visually inspecting those histograms.
+% ===================================================================
+
 % Clear all workspace variables,
 % Close any open windows and
 % clear the command line.
@@ -5,214 +19,88 @@ clear all; close all; clc;
 
 % ------------------------ Script Parameters ------------------------
 
-% Hyperparameters for 
-% estimating the bicoherence
+% Hyperparameters for estimating the bicoherence
 K  = 24;                        % Number of segments
 fs = 256;                       % Sampling frequency
 fc = 32;                        % upper bound on frequency axis
 
-% Select patients and an EEG
-% channels from the dataset
+% Select patients and an EEG channel from the dataset
 channel = 4;                    % EEG channel
 start = 1;                      % first patient
 stop = 50;                      % last patient
 
 % Plot settings
 nbins = 100;                    % Number of histogram bins
-norm = "pdf";                   % Normalization type for histograms:
+norm = "pdf";                   % Normalization type for histograms
                                 % cdf => cumulative distribution 
                                 % pdf => probability density function
 
 % ---------------- Do not change anything below ---------------------
 
 % Initialize empty tables to store bicoherence features
-types = ["double" "double" "double" "double" "string"];
-names = ["ent1" "ent2" "avg" "argmax" "Annotations"];
+types = ["double" "double" "double" "string"];
+names = ["ent1" "ent2" "avg" "Annotations"];
 sz = [0 numel(names)];
-delta = table('Size',sz,'VariableTypes',types,'VariableNames',names);
-theta = table('Size',sz,'VariableTypes',types,'VariableNames',names);
-alpha = table('Size',sz,'VariableTypes',types,'VariableNames',names);
-beta  = table('Size',sz,'VariableTypes',types,'VariableNames',names);
+
+Y = table('Size',sz,'VariableTypes',types,'VariableNames',names);
 
 % Extract bicoherence features from every patient
 for i = start:stop
+    % Make sure the input file exists
     txt = sprintf("SN%03d.edf",i);
     if ~isfile(txt) continue; end
 
+    % Load the EEG recordings 
     fprintf("Loading EDF files for patient %d ... ",i);
     Z = loadEDF(i);
     fprintf("OK\n");
 
+    % Choose an EEG channel and estimate the bicoherence
     fprintf("Estimating bicoherence matrix ... ");
     [X,f] = bicEEG(Z,K,fs,fc,channel,"fast");
     fprintf("OK\n");
 
+    % Extract features from every region of the 
+    % bicoherence matrices
     fprintf("Extracting bicoherence features ... ");
-    [D, U, A, B] = bicoherFeatures(X,f);
+    Y = [Y; bicoherFeatures(X,f)];
+    % [D, U, A, B] = bicoherFeatures(X,f);
     fprintf("OK\n");
-
-    delta = [delta; D];
-    theta = [theta; U];
-    alpha = [alpha; A];
-    beta  = [beta;  B];
 
     fprintf("\n");
 end
 
+% ----------------- Histograms of bicoherence features -----------------
+
+% incremental counter to distinguish between different plots and figures
 idx = 1;
 
-% --------------- Histograms for delta-wave features -----------------
+% binary arrays to split features based on sleep stage annotations
+W  = Y.Annotations == "Sleep stage W";
+R  = Y.Annotations == "Sleep stage R";
+N1 = Y.Annotations == "Sleep stage N1";
+N2 = Y.Annotations == "Sleep stage N2";
+N3 = Y.Annotations == "Sleep stage N3";
 
-W  = delta.Annotations == "Sleep stage W";
-R  = delta.Annotations == "Sleep stage R";
-N1 = delta.Annotations == "Sleep stage N1";
-N2 = delta.Annotations == "Sleep stage N2";
-N3 = delta.Annotations == "Sleep stage N3";
+K = size(Y,2) - 1;
 
-K = size(delta,2) - 1;
-
+% Plot histograms for every feature
 for k = 1:K
-    z1 = delta{W,  k};
-    z2 = delta{R,  k};
-    z3 = delta{N1, k};
-    z4 = delta{N2, k};
-    z5 = delta{N3, k};
-
-    if k == 2
-        z1 = log2(1+7*z1);
-        z2 = log2(1+7*z2);
-        z3 = log2(1+7*z3);
-        z4 = log2(1+7*z4);
-        z5 = log2(1+7*z5);
-    end
+    z1 = Y{W,k};        % Features of sleep stage W
+    z2 = Y{R,k};        % Features of sleep stage R
+    z3 = Y{N1,k};       % Features of sleep stage N1
+    z4 = Y{N2,k};       % Features of sleep stage N2
+    z5 = Y{N3,k};       % Features of sleep stage N3
 
     [y1, x1] = hist(z1,nbins); y1 = y1 / numel(z1);
     [y2, x2] = hist(z2,nbins); y2 = y2 / numel(z2);
     [y3, x3] = hist(z3,nbins); y3 = y3 / numel(z3);
     [y4, x4] = hist(z4,nbins); y4 = y4 / numel(z4);
     [y5, x5] = hist(z5,nbins); y5 = y5 / numel(z5);
-
+ 
     figure(idx); idx = idx + 1;
     plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5); grid on;
-    xlabel(delta.Properties.VariableNames{k}); 
+    xlabel(Y.Properties.VariableNames{k}); 
     ylabel('Probability');
-    title('Delta waves');
-    legend("W", "R", "N1", "N2", "N3");
-end
-
-% --------------- Histograms for theta-wave features -----------------
-
-W  = theta.Annotations == "Sleep stage W";
-R  = theta.Annotations == "Sleep stage R";
-N1 = theta.Annotations == "Sleep stage N1";
-N2 = theta.Annotations == "Sleep stage N2";
-N3 = theta.Annotations == "Sleep stage N3";
-
-K = size(theta,2) - 1;
-
-for k = 1:K
-    z1 = theta{W,  k};
-    z2 = theta{R,  k};
-    z3 = theta{N1, k};
-    z4 = theta{N2, k};
-    z5 = theta{N3, k};
-
-    if k == 2
-        z1 = log2(1+7*z1);
-        z2 = log2(1+7*z2);
-        z3 = log2(1+7*z3);
-        z4 = log2(1+7*z4);
-        z5 = log2(1+7*z5);
-    end
-
-    [y1, x1] = hist(z1,nbins); y1 = y1 / numel(z1);
-    [y2, x2] = hist(z2,nbins); y2 = y2 / numel(z2);
-    [y3, x3] = hist(z3,nbins); y3 = y3 / numel(z3);
-    [y4, x4] = hist(z4,nbins); y4 = y4 / numel(z4);
-    [y5, x5] = hist(z5,nbins); y5 = y5 / numel(z5);
-
-    figure(idx); idx = idx + 1;
-    plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5); grid on;
-    xlabel(theta.Properties.VariableNames{k}); 
-    ylabel('Probability');
-    title('theta waves');
-    legend("W", "R", "N1", "N2", "N3");
-end
-
-% --------------- Histograms for alpha-wave features -----------------
-
-W  = alpha.Annotations == "Sleep stage W";
-R  = alpha.Annotations == "Sleep stage R";
-N1 = alpha.Annotations == "Sleep stage N1";
-N2 = alpha.Annotations == "Sleep stage N2";
-N3 = alpha.Annotations == "Sleep stage N3";
-
-K = size(alpha,2) - 1;
-
-for k = 1:K
-    z1 = alpha{W,  k};
-    z2 = alpha{R,  k};
-    z3 = alpha{N1, k};
-    z4 = alpha{N2, k};
-    z5 = alpha{N3, k};
-
-    if k == 2
-        z1 = log2(1+7*z1);
-        z2 = log2(1+7*z2);
-        z3 = log2(1+7*z3);
-        z4 = log2(1+7*z4);
-        z5 = log2(1+7*z5);
-    end
-
-    [y1, x1] = hist(z1,nbins); y1 = y1 / numel(z1);
-    [y2, x2] = hist(z2,nbins); y2 = y2 / numel(z2);
-    [y3, x3] = hist(z3,nbins); y3 = y3 / numel(z3);
-    [y4, x4] = hist(z4,nbins); y4 = y4 / numel(z4);
-    [y5, x5] = hist(z5,nbins); y5 = y5 / numel(z5);
-
-    figure(idx); idx = idx + 1;
-    plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5); grid on;
-    xlabel(alpha.Properties.VariableNames{k}); 
-    ylabel('Probability');
-    title('alpha waves');
-    legend("W", "R", "N1", "N2", "N3");
-end
-
-% --------------- Histograms for beta-wave features -----------------
-
-W  = beta.Annotations == "Sleep stage W";
-R  = beta.Annotations == "Sleep stage R";
-N1 = beta.Annotations == "Sleep stage N1";
-N2 = beta.Annotations == "Sleep stage N2";
-N3 = beta.Annotations == "Sleep stage N3";
-
-K = size(beta,2) - 1;
-
-for k = 1:K
-    z1 = beta{W,  k};
-    z2 = beta{R,  k};
-    z3 = beta{N1, k};
-    z4 = beta{N2, k};
-    z5 = beta{N3, k};
-
-    if k == 2
-        z1 = log2(1+7*z1);
-        z2 = log2(1+7*z2);
-        z3 = log2(1+7*z3);
-        z4 = log2(1+7*z4);
-        z5 = log2(1+7*z5);
-    end
-
-    [y1, x1] = hist(z1,nbins); y1 = y1 / numel(z1);
-    [y2, x2] = hist(z2,nbins); y2 = y2 / numel(z2);
-    [y3, x3] = hist(z3,nbins); y3 = y3 / numel(z3);
-    [y4, x4] = hist(z4,nbins); y4 = y4 / numel(z4);
-    [y5, x5] = hist(z5,nbins); y5 = y5 / numel(z5);
-
-    figure(idx); idx = idx + 1;
-    plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5); grid on;
-    xlabel(beta.Properties.VariableNames{k}); 
-    ylabel('Probability');
-    title('beta waves');
     legend("W", "R", "N1", "N2", "N3");
 end
