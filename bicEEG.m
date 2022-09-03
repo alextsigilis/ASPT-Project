dd3cfa2839aeda0f3d9a3389e98e7afba07bcc53
Christodoulos Michailidis <cmichailidis98@gmail.com>
2022-09-03 05:43:54 +0300

    final commit (plus vectorization improvements)

--- bicEEG.m
+++ bicEEG.m
@@ -40,7 +40,7 @@
 %
 % fc: (float) The upper limit of the frequency axis.
 % Since it is rarely necessary to estimate the
-% bispectrum in the for every pair of frequencies
+% bispectrum for every pair of frequencies
 % -fs/2 < f1,f2 < +fs/2, we can greatly reduce the 
 % execution time and memory requirements by truncating
 % the frequency axis. Always make sure that fs and 
@@ -71,10 +71,9 @@
 % freq: (array of floats) A 1D array which can be used as a 
 % frequency axis for the bicoherence matrix. The size and the 
 % bounds of freq depend on fs, fc, K and method.
-% 
 % ===================================================================
 
-function [bic, freq] = bicEEG(X, K, fs, fc, channel, method)
+function [bic, freq] = bicEEGfast(X, K, fs, fc, channel, method)
 
     % ---------------------------------------------------------------
     % Parameter Checks
@@ -84,7 +83,7 @@ function [bic, freq] = bicEEG(X, K, fs, fc, channel, method)
     if fs <= 0 error("fs must be positive"); end
     if fc <= 0 error("fc must be positive"); end
     if channel < 1 || channel > 4 error("invalid EEG channel"); end 
-    
+
     if method ~= "fancy" && method ~= "fast" 
         error("Invalid estimation method");
     end
@@ -99,19 +98,15 @@ function [bic, freq] = bicEEG(X, K, fs, fc, channel, method)
     if M < 5 error("low frequency resolution"); end
     if 2*M*fc >= (M-2)*fs error("Nyquist criterion is violated"); end
     if fc*M <= fs error("fc is almost zero"); end
-    
+
     % ---------------------------------------------------------------
     % Create a table to store the bicoherence and sleep stage labels
     % ---------------------------------------------------------------
     
     names = [X.Properties.VariableNames{channel}, "Annotations"];
     types = ["cell", "string"];
-    
-    bic = table(                    ...
-        'Size',          [N 2],     ...
-        'VariableTypes', types,     ...
-        'VariableNames', names);
-    
+    sz = [N numel(types)];
+    bic = table('Size',sz,'VariableTypes',types,'VariableNames',names);
     bic.Annotations = X.Annotations;
     
     % ---------------------------------------------------------------
@@ -129,126 +124,140 @@ function [bic, freq] = bicEEG(X, K, fs, fc, channel, method)
     % ---------------------------------------------------------------
 
     % idx: array of indices for discarding unnecessary FFT components
+	% 
     % len: length of truncated FFT
+	% 
     % win: hanning window for FFT
+	% 
     % tri: array of indices for accumulating triple products
-    idx = 1:M; win = hanning(M);
+	% 
+    % seg: array of indices for partitioning the EEG records
+	% 
+    % hex: hexagonal boolean mask to remove artifacts outside the 
+	% symmetry regions
+	% 
+	% epsilon: a small positive constant to ensure numerical
+	% stability when performing floating point divisions
+		
+	idx = 1:M; win = hanning(M);
     
     if method == "fancy"
         idx = idx(-fc * M <= freq * fs & freq * fs <= fc * M);
-
     elseif method == "fast"
         idx = idx(freq * fs <= fc * M & freq >= 0);
     end
-
-    len = numel(idx);
-    tri = hankel([1:len],[len,1:len-1]);
+    
+	len = numel(idx);
+    
+	tri = hankel([1:len],[len,1:len-1]);
+	tri = reshape(tri, [len len 1]) + reshape(len * [0:K-1], [1 1 K]);
+    
+	seg = [1:M]' + M*[0:K-1];
 
     if method == "fancy"
-        % Hexagonal mask to remove artifacts
-        % outside the hexagonal symmetry regions
-        lo = -(len-1)/2; hi = +(len-1)/2; u = lo:1:hi;
-        hex = ones(len,1) * u;
-        hex = abs(hex) + abs(hex') + abs(hex+hex');
-        hex = (hex < len);
-
+        u = (1-len)/2:1:(len-1)/2;
+        u = ones(len,1) * u;
+        u = abs(u) + abs(u') + abs(u + u');
+        hex = u < len;
     elseif method == "fast"
-        % Triangular mask to remove artifacts
-        % outside the primary region
         u   = 0:1:(len-1);                                   
         u   = ones(len,1) * u;
         hex = (u' <= u) & (u >= 0) & (u + u' < len);
     end
 
-    % Bicoherence estimation for every 30sec epoch
-    for i = 1:N
-        % x: (1D array) a 30sec EEG recording
-        % b: (2D array) bispectrum/bicoherence matrix
-        x = cell2mat(X{i,channel}); 
-        b = zeros(len,len); 
-    
-        % P12, P3: (2D array) normalization coefficients
-        P12 = zeros(len,len); 
-        P3  = zeros(len,len);
-    
-        % ind:  (1D array) array of indices
-        % to extract EEG segments
-        ind  = [1:M];
-    
-        % A small positive constant to ensure numerical 
-        % stability when performing floating point divisions
-        epsilon = 1e-5;
-    
-        if method == "fancy"
-            % Partial estimations for every sub-segment
-            for j = 1:K
-                % Extract a segment from x
-                y = x(ind);
+    epsilon = 1e-5;
 
-                % Subtract the mean and
-                % apply the hanning window
-    	        y = ((y(:) - mean(y))/(std(y) + epsilon)) .* win;
-    
-                % Estimate the FFT of the segment
-                % and discard unnecessary frequencies
-    	        Y  = fft(y) / M;
-                Y  = fftshift(Y);
-                Y  = Y(idx);
-                Y  = ifftshift(Y);
-                CY = conj(Y);
+    % Estimate the bispectrum of every 30sec EEG record
+    if method == "fancy"
+        for i = 1:1:N
+            % Extract a 30sec EEG record
+            x = cell2mat(X{i,channel});
 
-                % Update the estimation of the power-spectrum
-                % Update the estimation of the bispectrum
-                Y12 = Y * Y.';
-                Y3  = CY(tri);
-                P12 = P12 + abs(Y12) .^ 2;
-                P3  = P3 + abs(Y3) .^ 2;
-                b   = b + Y12 .* Y3;
+            % Split, standardize and apply a window function
+            y = x(seg);
+            y = (y - mean(y,1)) ./ (std(y,0,1) + epsilon);           
+            y = y .* win;
 
-                % update the slicing indices
-                ind = ind + M;
-            end
-    
-            % Normalize the bispectrum to obtain the bicoherence
-            epsilon = 1e-5;
-            b = (abs(b) .^ 2) ./ (P12 .* P3 + epsilon);
-        
-            % Shift the elements of the bicoherence matrix,
-            % remove any artifacts outside the symmetry regions
-            % and save the final result.
-            bic{i,1} = {fftshift(b) .* hex};
-    
-        elseif method == "fast"
-            for j = 1:K
-                % Extract a segment from x
-                y = x(ind);
-                
-                % Subtract the mean and
-                % apply the hanning window                
-                y = (y(:) - mean(y)) / (std(y) + epsilon) .* win;
-    
-                % Estimate the FFT of the segment
-                % and discard unnecessary frequencies
-                Y  = fft(y) / M;
-                Y  = fftshift(Y);
-                Y  = Y(idx);
-                CY = conj(Y);
-    
-                % Update the estimation of the power-spectrum
-                % Update the estimation of the bispectrum
-                Y12 = Y * Y.';
-                Y3  = CY(tri);
-                P12 = P12 + abs(Y12) .^ 2;
-                P3  = P3 + abs(Y3) .^ 2;
-                b   = b + Y12 .* Y3;
-    
-                % update the slicing indices 
-                ind = ind + M;
-            end
+            % estimate the FFT of every segment
+            % and discard unnecessary frequencies
+            Y = fft(y,[],1) / M;
+            Y = fftshift(Y,1);
+            Y = Y(idx,:);
+            Y = ifftshift(Y,1);
+            CY = conj(Y); CY = CY(tri);
 
-            % Normalize the bispectrum to obtain the bicoherence
-            epsilon = 1e-5;
-            b = (abs(b) .^ 2) ./ (P12 .* P3 + epsilon);
+            % Reshape the FFT matrix appropriately in order
+            % to vectorize the following calculations
+            Y1 = reshape(Y, [len 1 K]);                                           
+            Y2 = reshape(Y, [1 len K]);
+
+            % Estimate the bispectrum (b) and the 
+            % normalization coefficients (Y12, CY)
+            Y12 = Y1 .* Y2; 
+            b   = Y12 .* CY;
+
+            % Normalize the bispectrum matrix to obtain
+            % the bicoherence.
+            Y12 = abs(Y12) .^ 2; 
+            Y12 = sum(Y12, 3);
+
+            CY = abs(CY) .^ 2;
+            CY = sum(CY, 3);
+
+            b = sum(b, 3);
+            b = abs(b) .^ 2;
+
+            b = b ./ (Y12 .* CY + epsilon);
+
+            % Shift the elements of the bispectrum matrix,
+            % discard any elements outside the symmetry 
+            % regions and save the final result.
+            b = fftshift(b);
+            bic{i,1} = {b .* hex};
+        end
+    
+    elseif method == "fast"
+        for i = 1:1:N
+            % Extract a 30sec EEG record
+            x = cell2mat(X{i,channel});
+
+            % Split, standardize and apply a window function
+            y = x(seg);
+            y = (y - mean(y,1)) ./ (std(y,0,1) + epsilon);           
+            y = y .* win;
+
+            % estimate the FFT of every segment
+            % and discard unnecessary frequencies
+            Y = fft(y,[],1) / M;
+            Y = fftshift(Y,1);
+            Y = Y(idx,:);
+            CY = conj(Y); CY = CY(tri);
+
+            % Reshape the FFT matrix appropriately in order
+            % to vectorize the following calculations
+            Y1 = reshape(Y, [len 1 K]);                                           
+            Y2 = reshape(Y, [1 len K]);
+
+            % Estimate the bispectrum (b) and the 
+            % normalization coefficients (Y12, CY)
+            Y12 = Y1 .* Y2; 
+            b   = Y12 .* CY;
+
+            % Normalize the bispectrum matrix to obtain
+            % the bicoherence.
+            Y12 = abs(Y12) .^ 2; 
+            Y12 = sum(Y12, 3);
+
+            CY = abs(CY) .^ 2;
+            CY = sum(CY, 3);
+
+            b = sum(b, 3);
+            b = abs(b) .^ 2;
+
+            b = b ./ (Y12 .* CY + epsilon);
+
+            % Discard any elements outside the symmetry 
+            % regions and save the final result.
             bic{i,1} = {b .* hex};
         end
     end
