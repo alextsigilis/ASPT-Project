385a9890924a16709fa69fd95ac6afb649f8cfc5
cmichailidis <cmichailidis98@gmail.com>
2022-09-13 16:15:17 +0300

    fixed minor bugs and improved code readability

--- weightHist.m
+++ weightHist.m
@@ -18,31 +18,8 @@ stages = [              ...
     "Sleep stage N2",   ...
     "Sleep stage N3"]; 
 
-% K: (integer) number of features in QPC table
 % M: (integer) number of sleep stages
-% N: (integer) number of EEG wave types
-K = 9;  M = 5; N = 4;
-
-% F: array of frequency bounds for delta, theta, alpha and beta waves
-% coeff: array of normalization factors for weighted histogram
-F = [0.0; 4.0; 8.0; 16.0; 32.0];
-% coeff = [];
-
-% Estimate normalization coefficients for 
-% every region of the bicoherence matrices
-% for n = 1:1:4                 
-%     for m = n:1:4
-%         u = (f > F(n)) & (f <= F(n+1));
-%         v = (f > F(m)) & (f <= F(m+1));             
-%         w = v & u' & (f >= f') & (f + f' <= F(end));
-%         coeff = [coeff sum(w(:))];
-%     end
-% end
-
-% Discard unnecessary frequencies
-if any(f < 0) 
-    f = f(f > F(0) & f <= F(end));
-end
+M = numel(stages);
 
 % Iterate through all sleep stages
 for m = 1:1:M                  
@@ -50,38 +27,35 @@ for m = 1:1:M
     idx = masks{m};
 
     % Histogram array
-    h = zeros(numel(f));
+    h = zeros(numel(f), numel(f));
 
-    % Iterate through all bicoherence sub-regions
-    for k = 1:1:K
-        % X: sub-array of QPC features
-        X = cell2mat(QPC{idx,k});
+    % X: sub-array of QPC features
+    X = cell2mat(QPC{idx,"peaks"});
 
-        % f1: x-coordinates of histogram
-        % f2: y-coordinates of histogram
-        % b:  b(f1,f2) "weight" of histogram observations
-        f1 = X(:,1);                
-        f2 = X(:,2);                
-        b  = X(:,3) / sum(X(:,3));  
+    % f1: x-coordinates of histogram
+    % f2: y-coordinates of histogram
+    % b:  b(f1,f2) "weight" of histogram observations
+    f1 = X(:,1);                
+    f2 = X(:,2);                
+    b  = X(:,3) / sum(X(:,3));  
 
-        % update weighted histogram
-        for l = 1:1:size(X,1)
-            x = f1(l); y = f2(l); z = b(l);
-            h(x,y) = h(x,y) + z;
-        end
+    % update weighted histogram
+    for l = 1:1:size(X,1)
+        x = f1(l); y = f2(l); z = b(l);
+        h(x,y) = h(x,y) + z;
     end
 
-    % Plot weighted histogram of QPC pairs
-    figure(m);
-    contourf(f,f,h,16,'LineColor','none');
-    
-    xlabel("f_1"); 
-    ylabel("f_2"); 
-    title(sprintf("weighted QPC histograms, %s",stages(m)));
+% Plot weighted histogram of QPC pairs
+figure(m);
+contourf(f,f,h,16,'LineColor','none');
+  
+xlabel("f_1"); 
+ylabel("f_2"); 
+title(sprintf("weighted QPC histograms, %s",stages(m)));
 
-    caxis([0 1]);
-    colormap("turbo");
-    colorbar;
+caxis([0 1]);
+colormap("turbo");
+colorbar;
 end
 
 end
