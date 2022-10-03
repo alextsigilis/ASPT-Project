07b7e60fc4f3c058cca929adc157e1e098653f18
cmichailidis <cmichailidis98@gmail.com>
2022-10-03 21:14:53 +0300

    second draft

--- extractAll.m
+++ extractAll.m
@@ -31,7 +31,7 @@ clc;            % Clear the terminal
 % ===================================================================
 
 first = 1;          % First patient selected from the dataset
-last  = 20;         % Last patient selected from the dataset
+last  = 1;         % Last patient selected from the dataset
 
 fs = 256;           % Sampling frequency of PSG recordings (in Hertz)
 dt = 30;            % Epoch duration of PSG recordings (in seconds)
@@ -47,8 +47,6 @@ path = sprintf("C:\\Users\\USER\\Desktop\\features");
 % ===================================================================
 
 for n = first:1:last
-    tic;
-
     % Make sure that the EDF/mat file exists
     edfFile = sprintf("SN%03d.edf",n);
     matFile = sprintf("%03d.mat",n);
@@ -70,6 +68,11 @@ for n = first:1:last
     Z = prefilter(Z,fs);
     fprintf("Done\n");
 
+    % X: (2D array) Array of PSG features
+    % y: (1D array) Array of Sleep stage Annotations
+    X = [];
+    y = table2array(Z(:,end));
+
     % ---------------- DWT Features ----------------
     
     fprintf("Performing MRA decomposition ... ");
@@ -86,7 +89,26 @@ for n = first:1:last
     [D4, T4, A4, B4] = statEEG(coeff4);
     fprintf("Done\n");
 
-    % TODO: store features in tables
+    % Copy DWT features to X
+    X = [X table2array(D1(:,1:end-1))]; 
+    X = [X table2array(T1(:,1:end-1))]; 
+    X = [X table2array(A1(:,1:end-1))]; 
+    X = [X table2array(B1(:,1:end-1))];
+    
+    X = [X table2array(D2(:,1:end-1))]; 
+    X = [X table2array(T2(:,1:end-1))]; 
+    X = [X table2array(A2(:,1:end-1))]; 
+    X = [X table2array(B2(:,1:end-1))];
+    
+    X = [X table2array(D3(:,1:end-1))]; 
+    X = [X table2array(T3(:,1:end-1))]; 
+    X = [X table2array(A3(:,1:end-1))]; 
+    X = [X table2array(B3(:,1:end-1))];
+    
+    X = [X table2array(D4(:,1:end-1))]; 
+    X = [X table2array(T4(:,1:end-1))]; 
+    X = [X table2array(A4(:,1:end-1))]; 
+    X = [X table2array(B4(:,1:end-1))];
 
     % ------------ Bicoherence Features ------------
 
@@ -104,7 +126,11 @@ for n = first:1:last
     Y4 = bicoherFeatures(b4,f4);
     fprintf("Done\n");
 
-    % TODO: store features in tables
+    % Copy bicoherence features to X
+    X = [X table2array(Y1(:,1:end-1))]; 
+    X = [X table2array(Y2(:,1:end-1))]; 
+    X = [X table2array(Y3(:,1:end-1))]; 
+    X = [X table2array(Y4(:,1:end-1))];
 
     % ---------------- QPC features ----------------
     fprintf("Locating QPC frequency-pairs ... ");
@@ -121,21 +147,29 @@ for n = first:1:last
     Y4 = QPCfeatures(qpc4,f4);
     fprintf("Done\n");
 
-    % TODO: store features in tables
+    % Copy QPC features to X
+    X = [X table2array(Y1(:,1:end-1))]; 
+    X = [X table2array(Y2(:,1:end-1))]; 
+    X = [X table2array(Y3(:,1:end-1))]; 
+    X = [X table2array(Y4(:,1:end-1))];
 
     % ------------- Cepstrum Features --------------
-    fprintf("Extracting cepstral features ... ");
+    % fprintf("Extracting cepstral features ... ");
     % rc1 = rcepFeatures(Z, "EEGF4_M1");
     % rc2 = rcepFeatures(Z, "EEGC4_M1");
     % rc3 = rcepFeatures(Z, "EEGO2_M1");
     % rc4 = rcepFeatures(Z, "EEGC3_M2");
-    fprintf("Done\n");
+    % fprintf("Done\n");
 
-    toc;
-
-    % TODO: store features in tables
+    % Copy cepstral features to X
+    % X = [X table2array(rc1(1:end-1))]; 
+    % X = [X table2array(rc2(1:end-1))]; 
+    % X = [X table2array(rc3(1:end-1))];
+    % X = [X table2array(rc4(1:end-1))]; 
 
     % ---------------- EOG features ----------------
 
     % ---------------- EMG features ----------------
+
+    fprintf("\n\n");
 end
